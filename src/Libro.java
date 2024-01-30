cdd1d7a7442813cba1ac327bedeea971ff7f4618
Elisa7584 <elisaortega112.alu@stemgranada.com>
2024-01-30 11:56:38 +0100

    clase Libro creada

--- src/Libro.java
+++ src/Libro.java
@@ -33,28 +33,33 @@ public class Libro {
         this.numPaginas = 0;
     }
 
+
     public String getTitulo() {
         return titulo;
     }
-
-
-    public int getISBN() {
-        return ISBN;
-    }
-
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
-
     public String getAutor() {
         return autor;
     }
 
-
     public void setAutor(String autor) {
         this.autor = autor;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
     }
 
     //Siempre se tirne que llamar toString
